3e7a7aafbeddfbf03d0cc3d3a533fb8868380bde
Dalton Phillips <daltonphillips1020@gmail.com>
2022-01-14 00:20:22 -0500

    UI updates

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,11 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import Video from "../../videos/video.mp4";
+import { Button } from "../mint";
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight,
+} from "./HeroElements";
 
 const HeroSection = () => {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  };
+
   return (
     <HeroContainer>
       <HeroBg>
         <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
       </HeroBg>
+      <HeroContent>
+        <HeroH1>PsychPunks</HeroH1>
+        <HeroP>
+          Mint a psychpunks today! And still have a change to get a Psychpunks
+          hoodie!
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button
+            to="mint"
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+            primary="true"
+            dark="true"
+          >
+            Mint {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
     </HeroContainer>
   );
 };
