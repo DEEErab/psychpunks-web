0ab922d715bac38b44e63964e8568470d608521a
Dalton Phillips <daltonphillips1020@gmail.com>
2022-01-24 18:59:38 -0500

    font size

--- src/components/About/AboutElements.js
+++ src/components/About/AboutElements.js
@@ -64,7 +64,7 @@ export const TopLine = styled.p`
 `;
 export const Heading = styled.h1`
   margin-bottom: 24px;
-  font-size: 48;
+  font-size: 48px;
   line-height: 1.1;
   font-weight: 600;
   color: white;
